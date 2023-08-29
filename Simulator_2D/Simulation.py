e6b077427220bba6953e7cede9d0bced54cb3875
Jakob <jakob.d.neuhauser@gmail.com>
2023-08-29 17:57:20 +0200

    some code commented.

--- Simulator_2D/Simulation.py
+++ Simulator_2D/Simulation.py
@@ -1,13 +1,20 @@
 import numpy as np
 import math
 
+
 class Simulation:
 
-    # Constructor
-    ##################################
-    def __init__(self, Grid, gravity, Potential=lambda x=0: 0):
-        self.grid = Grid
-        self.potential = Potential
+    def __init__(self, grid, gravity, potential=lambda x=0: 0):
+        """
+        Class that carries out the actual simulation.
+
+        :param grid: The grid on which the simulation is carried out.
+        :param potential: The potential function used for the simulation. (default 0)
+        :param gravity: The "gravity" of the system. (How much the waves attract one another.)
+        """
+
+        self.grid = grid
+        self.potential = potential
         self.gravity = gravity
 
     # Some wrapper functions
@@ -38,58 +45,61 @@ class Simulation:
         dt = self.grid.grid_parameters.time_step_size
 
         # define operators (which are actually vectors)
-        ###############################################################
+        # -------------------------------------------------------------
         xv, yv = np.meshgrid(self.grid.x_axis, self.grid.y_axis)
-        V = self.potential(xv, yv)
-        opr_R = np.power(math.e, -1 * 1j * V * dt)
+        v = self.potential(xv, yv)
+        opr_r = np.power(math.e, -1 * 1j * v * dt)
 
-        kx = np.fft.fftfreq(self.grid.grid_parameters.space_steps_X) * (
-                    self.grid.grid_parameters.space_steps_X / 2) * math.pi / self.grid.x_axis[
-                 -1]  # From k = (2*pi)/L | The 2 vanishes since the grid is devided in half.
-        ky = np.fft.fftfreq(self.grid.grid_parameters.space_steps_Y) * (
-                    self.grid.grid_parameters.space_steps_Y / 2) * math.pi / self.grid.y_axis[
-                 -1]  # From k = (2*pi)/L | The 2 vanishes since the grid is devided in half.
+        # Info: From k = (2*pi)/L | The 2 vanishes since the grid is divided in half.
+        space_steps_x = self.grid.grid_parameters.space_steps_X
+        kx = np.fft.fftfreq(space_steps_x) * (space_steps_x / 2) * math.pi / self.grid.x_axis[-1]
+
+        # Info: From k = (2*pi)/L | The 2 vanishes since the grid is divided in half.
+        space_steps_y = self.grid.grid_parameters.space_steps_Y
+        ky = np.fft.fftfreq(space_steps_y) * (space_steps_y / 2) * math.pi / self.grid.y_axis[-1]
+
         k = np.meshgrid(kx, ky)
         k = np.array(k)
         k = np.sqrt(np.power(k[0], 2) + np.power(k[1], 2))
         k[0][0] = 0.0001
 
-        opr_K = np.power(math.e, -0.5 * 1j * np.power(k, 2) * dt)
+        opr_k = np.power(math.e, -0.5 * 1j * np.power(k, 2) * dt)
 
         # meshgrid for 2D-Potential
-        #################################
-        X, Y = np.meshgrid(self.grid.x_axis.real, self.grid.y_axis.real)
+        # ------------------------
+        x, y = np.meshgrid(self.grid.x_axis.real, self.grid.y_axis.real)
 
-        # set the first enery
-        #################################
+        # set the first energy
+        # ------------------------
         self.grid.energy[0] = np.sum(np.square(np.abs(self.grid.grid[0])))
 
         # iterate
-        #####################
+        # ------------------------
         for i in range(0, self.grid.grid_parameters.time_steps - 1):
+
             # FFT
-            ############################
+            # ------------------------
             tmp = np.fft.fft2(self.grid.grid[i])
-            V = np.fft.fft2(np.abs(np.power(self.grid.grid[i], 2)))
+            v = np.fft.fft2(np.abs(np.power(self.grid.grid[i], 2)))
 
             # Poisson
-            ############################
-            V = (-1 / (np.power(np.abs(k), 2))) * V  # (np.power(np.abs(k)
+            # ------------------------
+            v = (-1 / (np.power(np.abs(k), 2))) * v  # (np.power(np.abs(k)
 
             # Momentum
-            ############################
-            tmp = tmp * opr_K
+            # ------------------------
+            tmp = tmp * opr_k
 
             # IFFT
-            ############################
+            # ------------------------
             tmp = np.fft.ifft2(tmp)
-            V = np.fft.ifft2(V)
+            v = np.fft.ifft2(v)
 
             # Position
-            ############################
-            V = V * self.gravity + self.potential(X, Y)
-            opr_R = np.power(math.e, -1 * 1j * V * dt)
-            tmp = tmp * opr_R
+            # ------------------------
+            v = v * self.gravity + self.potential(x, y)
+            opr_r = np.power(math.e, -1 * 1j * v * dt)
+            tmp = tmp * opr_r
 
             self.grid.grid[i + 1] = tmp
 
